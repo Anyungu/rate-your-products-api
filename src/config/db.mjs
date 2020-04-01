963ad02f47760140c33f71552b76d0b6e9712ecc
Anyungu <anyungucw@gmail.com>
2020-04-01 21:46:52 +0300

    git cache cleared

--- src/config/db.mjs
+++
@@ -1,9 +0,0 @@
-
-
-
-import mongoose from 'mongoose';
-
-
-mongoose.PromiseProvider = global.Promise;
-
-export const dbConnect = () => mongoose.connect('mongodb://ltkfinal:my%20password@168.62.174.233:27017/rateShoes?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false', {useNewUrlParser: true});
